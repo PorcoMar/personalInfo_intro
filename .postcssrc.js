9ba5842c10de831c06547176844212abb6f14747
Porco_Mar <990135141@qq.com>
2018-05-23 13:11:51 +0800

    commit fir

---
+++ .postcssrc.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Porco_Mar
+* @Date:   2018-05-23 13:08:51
+* @Last Modified by:   Porco_Mar
+* @Last Modified time: 2018-05-23 13:09:29
+*/
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {}
+  }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+
+## 简介
+### 初始化项目
+clone项目，进入dev分支
+
+    npm install
+### 运行
+    npm dev
+### 编译
+    npm build
+访问：http://localhost:8080/
